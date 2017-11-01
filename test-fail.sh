2d6439460bbf2cd7ab1cc8cb7afaed4514214477
Jack Firth <jackhfirth@gmail.com>
2017-11-01 01:14:42 -0700

    Add test script containing known failures
    These tests currently fail and need to be fixed in some way. Reasons
    for failure include known bugs and issues running inside a container,
    among other things.

---
+++ test-fail.sh
@@ -0,0 +1,23 @@
+# This script contains test commands that are currently known to fail due to
+# configuration issues or known bugs. They should be moved into the appropriate
+# `test.sh` or `test-gui.sh` script once fixed.
+
+# ryanc
+# fails due to the same failures seen on drdr
+raco test -l tests/macro-debugger/all-tests
+
+# stchang
+# currently fails because of check-random, fix is on master
+raco test -l tests/stepper/automatic-tests
+
+# mflatt
+# fails with something about -fPIC
+racket -l tests/racket/embed-in-c
+# fails with unicode / locale issues
+racket -l tests/compiler/embed/test
+# fails with an error about #f being passed to `system*`
+racket -l distro-build/tests/unix-installer
+# fails with claim that the /beginner.rktl file is missing; likely caused by
+# assumptions about the current directory (docker container uses root directory
+# by default)
+racket -l tests/htdp-lang/test-htdp
