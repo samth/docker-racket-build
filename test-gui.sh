b6d3721a24e0b22d6ed76d78473762be372f4b25
Jack Firth <jackhfirth@gmail.com>
2017-11-01 01:13:11 -0700

    Add non-functional GUI test script
    Doesn’t work yet. Requires a properly containerized xvfb server and
    installing several GUI-related foreign libraries.

---
+++ test-gui.sh
@@ -0,0 +1,34 @@
+# Like the `test.sh` script, but for GUI tests. These tests require a running X
+# server (run with xvfb for a fake X backend) and several GUI-related foreign
+# libraries. Test commands that are prefixed with "xvfb-run" should instead
+# rely on a single shared X server, since it's easier and more intuitive to run
+# one in its own Docker container that's shared for all tests.
+set -eufx
+
+# TODO: This script doesn't actually work yet. Commands that are known to fail
+# should be moved to the test-fail.sh script.
+
+# samth
+xvfb-run racket -l typed-racket-test -- --all
+
+# jay
+raco test -c tests/xml # unsure why this requires foreign GUI libs
+xvfb-run raco test -c plai
+
+# robby
+xvfb-run racket -l 2htdp/tests/bitmap-as-image-in-universe
+xvfb-run racket -l 2htdp/tests/image-equality-performance-htdp
+xvfb-run racket -l 2htdp/tests/image-equality-performance
+xvfb-run racket -l 2htdp/tests/image-too-large
+xvfb-run racket -l 2htdp/tests/test-image
+# fails due to font & rendering differences
+xvfb-run racket -l redex/tests/run-tests
+
+# mflatt
+# TODO: investigate why this needs its own directory, possibly use explicit
+# /tmpdir or edit test to avoid directory dependency entirely.
+mkdir test-dir
+cd test-dir
+xvfb-run racket -l tests/gracket/test
+xvfb-run gracket -z -e 1
+xvfb-run gracket -e 1
