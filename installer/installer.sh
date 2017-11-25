3bbf61fe8d98f1ad50294e9cc1f175e41197767a
Jack Firth <jackhfirth@gmail.com>
2017-11-24 16:40:59 -0800

    Simplify installer script with an alias for docker-compose

--- installer/installer.sh
+++ installer/installer.sh
@@ -7,5 +7,7 @@ INSTALLATION_TYPE=$2
 export INSTALLATION_TYPE
 export INSTALLER_TYPE
 
-docker-compose -f /etc/racket-installer/docker-compose.yml -p ${INSTALLER_TYPE} build --force-rm  ${INSTALLATION_TYPE}
-docker-compose -f /etc/racket-installer/docker-compose.yml -p ${INSTALLER_TYPE} run --rm ${INSTALLATION_TYPE} racket --version
+alias installer-compose="docker-compose -f /etc/racket-installer/docker-compose.yml -p ${INSTALLER_TYPE}"
+
+installer-compose build --force-rm  ${INSTALLATION_TYPE}
+installer-compose run --rm ${INSTALLATION_TYPE} racket --version
